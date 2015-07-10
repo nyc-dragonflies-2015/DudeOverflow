82ae1b7a940d9c993886b3b5ecc19ee36f33dcff
Joseph Fields <Joseph.Fields22@gmail.com>
2015-07-10 16:21:50 -0400

    created question seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+
+Question.create(title: "hey does anyone know how to use a computer",body: "this is the greatest question in the world how do I do this I realy don't know how to do this it seems very impossible")
+Question.create(title: "Mike is the biggest crybaby in the world",body: "I mean common how do you write a question in ruby I don't know how")
+Question.create(title: "Dude this is Bro overflow not Stackoverflow",body: "only dudes work at dude overflow MICROAGRESSION!")
