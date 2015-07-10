64ca26fdce940b824115335cf8ba0f5bbd03761e
Alex Satterly <alcsatt@gmail.com>
2015-07-10 13:28:00 -0400

    Add create action for answers

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,23 +1,29 @@
 class AnswersController < ApplicationController
 
   def create
+    question = Question.find(question_id: params[:question_id])
+    @answer = Answer.new(answer_params)
+    if @answer && @answer.save
 
+    else
+      flash[:alert] = "#{@answer.errors.full_messages.join("--")}"
+    end
+    redirect_to question
   end
 
-  def show
 
+
+  private
+
+  def answer_params
+    params.require(:answer).permit(:body, :user_id, :question_id)
   end
 
-  def edit
-
-  end
-
-  def update
-
-  end
-
-  def destroy
-
+  def verify_answer_ownership
+    unless @answer.user_id == current_user.id
+      flash[:notice] = "This answer does not belong to you."
+      redirect_to root_path
+    end
   end
 
 end
