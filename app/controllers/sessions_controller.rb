e8cd4c1158dda6dbe4c28095a235012f3c84e45f
Alex Satterly <alcsatt@gmail.com>
2015-07-10 11:46:44 -0400

    Finish sessions#new sessions#create and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,15 +1,28 @@
 class SessionsController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
-
+    @user = User.find_by(email: user_params[:email])
+    if @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      flash[:notice] = "Invalid Username or Password"
+      redirect_to login_path
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+  end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
   end
 
 end
