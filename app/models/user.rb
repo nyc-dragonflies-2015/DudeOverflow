21f84798a149cf115b40248758485ae524757870
Alex Satterly <alcsatt@gmail.com>
2015-07-10 10:12:16 -0400

    Create user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ActiveRecord::Base
+  has_secure_password
+  has_many :questions
+  has_many :answers
+  has_many :comments
+  has_many :votes
+
+  validates :username, presence: true, uniqueness: true
+  validates :password_digest, presence: true
 end
