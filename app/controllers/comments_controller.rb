b68029864cd558cef6d38e402e4b0e9c259c3f01
johnlyden <john.lyden6@gmail.com>
2015-07-10 16:32:48 -0400

    working question comment box that associates with question

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,12 +7,18 @@ class CommentsController < ApplicationController
     @comment = Comment.new(comment_params)
     @comment.update_attributes(user_id: current_user.id)
     if @comment.commentable_type == "Question"
-      @question = Question.find(@comment.commentable_id).id
+      @question = Question.find(@comment.commentable_id)
       @comment.commentable_id = @question.id
     elsif @comment.commentable_type == "Answer"
       @answer = Answer.find(@comment.commentable_id)
       @comment.commentable_id = @answer.id
     end
     @comment.save
+    redirect_to @question
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:body, :user_id, :commentable_id, :commentable_type)
   end
 end
