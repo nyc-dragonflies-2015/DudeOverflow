6abc4f51e8fdee18882e490907dc4a92e8607ade
Alex Satterly <alcsatt@gmail.com>
2015-07-10 14:34:02 -0400

    Change votes table to have boolean instead of count

--- db/migrate/20150709213435_create_votes.rb
+++ db/migrate/20150709213435_create_votes.rb
@@ -1,7 +1,7 @@
 class CreateVotes < ActiveRecord::Migration
   def change
     create_table :votes do |t|
-      t.integer :count, null: false, default: 0
+      t.boolean :upvote, null: false
       t.references :user
       t.references :voteable, polymorphic: true, index: true
       t.timestamps null: false
