c507abe0e61769ea9efcada2e0bd70ff11a619be
johnlyden <john.lyden6@gmail.com>
2015-07-10 11:38:27 -0400

    adding current_user and logged_in? helpers in application_helper

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,22 @@
+class QuestionController < ApplicationController
+
+  def index
+    @questions = Question.all
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def new
+    @question = Question.new
+  end
+
+  def create
+    @question = current_user.questions.create(params[:question])
+  end
+
+  def destroy
+
+  end
+end
