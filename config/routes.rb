a5b6da1f8386e21ec026851eda79070a265e6d3c
johnlyden <john.lyden6@gmail.com>
2015-07-10 12:32:17 -0400

    adding new question form partial and rendering partial in edit view, also adding questions resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,10 @@ Rails.application.routes.draw do
 
   get '/login' => 'sessions#new', as: 'login'
   post '/login' => 'sessions#create', as: 'create_session'
+
   delete '/logout' => 'sessions#destroy', as: 'logout'
   resources :users, only: [:new, :create]
+  resources :questions
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
