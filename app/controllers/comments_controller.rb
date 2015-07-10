200a6930c15c0c7ad0c320cb2d5904a6dfbc5d04
johnlyden <john.lyden6@gmail.com>
2015-07-10 16:05:22 -0400

    working on associating comments with questions or answers

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,14 @@
 class CommentsController < ApplicationController
-
   def new
     @comment = Comment.new
   end
 
-
-
+  def create
+    @comment = @commentable.comments.create(params[:comment])
+    if @comment.valid?
+      render text: "great"
+    else
+      flash[:alert] = "#{@comment.errors.full_messages.join("==")}"
+    end
+  end
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,3 +1,10 @@
 class VotesController < ApplicationController
 
+  def new
+    @vote = Vote.new
+  end
+
+  def create
+    @vote = current_user.votes.create(params[:vote])
+  end
 end
