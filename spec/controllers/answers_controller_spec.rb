99fc28bab527fd98c88220fd1ecf26dbed5b06a6
michaelbbozza <michaelbbozza@gmail.com>
2015-07-10 22:38:53 -0400

    answer controller, partially finished

---
+++ spec/controllers/answers_controller_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+describe AnswersController do
+  describe "GET show" do
+    it "should show an answer to a question" do
+      answer = Answer.create(body: "test")
+      byebug
+      get :show, id: answer.id
+      expect(response).to render_template("show")
+    end
+  end
+end
