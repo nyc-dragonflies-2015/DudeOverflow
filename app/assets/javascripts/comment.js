346154c63e7918ec4344f766c84f5864c0bab3d2
johnlyden <john.lyden6@gmail.com>
2015-07-11 14:57:19 -0400

    working ajax for comments on questions and answers

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -14,4 +14,24 @@ $(document).ready(function(){
       console.log(error)
     });
   });
+
+  $('.answer-comment-form').on('submit', function(event){
+    event.preventDefault();
+    console.log("yo");
+    var commentFormId = "#" + $(this).attr("id");
+    console.log(commentFormId);
+    $.ajax({
+      url: $(this).attr('action'),
+      method: 'POST',
+      data: $(this).serialize()
+    })
+    .done(function(response){
+      console.log(response);
+      $(commentFormId).after(response + "<br/>");
+      $(commentFormId)[0].reset();
+    })
+    .fail(function(error){
+      console.log(error);
+    })
+  })
 })
