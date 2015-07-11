f786296566ad7e8e722720d14cbd17a4d56b43bc
michaelbbozza <michaelbbozza@gmail.com>
2015-07-10 22:25:37 -0400

    finished questions controller tests

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -1,14 +1,12 @@
 require 'rails_helper'
 
 describe QuestionsController do
-  let(:db_array) {[]}
-  let(:question) {Question.create(title: "RSPEC", body: "TEST")}
-  let(:found_question) {Question.find(question.id)}
+  let(:q) {Question.create(title: "RSPEC", body: "TEST")}
 
   describe "GET index" do
     it "assigns @questions" do
       get :index
-      expect(assigns(:questions)).to eq([question])
+      expect(assigns(:questions)).to eq(Question.all)
     end
 
     it "renders index template" do
@@ -19,12 +17,12 @@ describe QuestionsController do
 
   describe "GET show" do
     it "assigns @question" do
-      get :show, id: found_question.id
-      expect(assigns(:question)).to eq(found_question)
+      get :show, id: q.id
+      expect(assigns(:question)).to eq(q)
     end
 
     it "should render a view for question" do
-      get :show, id: found_question.id
+      get :show, id: q.id
       expect(response).to render_template("show")
     end
   end
@@ -42,22 +40,28 @@ describe QuestionsController do
 
   describe "POST create" do
     it "should create a new Question" do
+      q
       get :new
-      db_array << question
-      expect(db_array.length).to eq(1)
+      expect(Question.count).to eq(1)
     end
   end
 
   describe "DELETE 'delete'" do
     it "should remove a question" do
-      q = Question.create(title: "test", body: "testing")
       q.destroy
       expect(Question.all).to eq([])
     end
 
-    it "should redirect to root path" do
-      q = Question.create(title: "test", body: "testing")
+    it "should reach root path" do
       expect(response).to have_http_status(200)
+    end
+  end
+
+  describe "GET edit" do
+    it "should show a form to edit a question" do
+      q
+      get :edit, id: q.id
+      expect(response).to render_template("edit")
     end
   end
 end
