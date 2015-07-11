c2f8e529a2ea999083d84770da423a2ce4ed6ae4
Alex Satterly <alcsatt@gmail.com>
2015-07-11 11:41:32 -0400

    Add vote helpers to answer and comment model

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,4 +3,16 @@ class Comment < ActiveRecord::Base
   belongs_to :commentable, polymorphic: true
 
   validates :body, presence: true
+
+  def vote_count
+    self.upvotes - self.downvotes
+  end
+
+  def upvotes
+    self.votes.where(upvote: true).count
+  end
+
+  def downvotes
+    self.votes.where(upvote: false).count
+  end
 end
