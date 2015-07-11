e960020fb90762681f3f33702eca9dc504b8efa1
johnlyden <john.lyden6@gmail.com>
2015-07-11 14:21:58 -0400

    adding working ajax call to create comments on each question

--- app/assets/javascripts/comment.js
+++ app/assets/javascripts/comment.js
@@ -1,16 +1,17 @@
 $(document).ready(function(){
   $('#question-comment-form').on('submit', function(event){
   event.preventDefault();
-  console.log($(this));
     $.ajax({
       url: 'http://localhost:3000/comments',
       method: 'POST',
       data: $(this).serialize()
-    }).done(function(response){
-      console.log(response);
-      $('ul#comment-list').append(response);
-    }).fail(function(error){
-      console.log('error')
+    })
+    .done(function(response){
+      $('#question-comments-header').after(response);
+      $('#question-comment-form')[0].reset();
+    })
+    .fail(function(error){
+      console.log(error)
     });
   });
 })
