6b14d016d42c3e40032534885597dc8447486bae
michaelbbozza <michaelbbozza@gmail.com>
2015-07-10 22:51:37 -0400

    one test for sessions cont, nothing changed in answer

--- spec/controllers/answers_controller_spec.rb
+++ spec/controllers/answers_controller_spec.rb
@@ -4,7 +4,6 @@ describe AnswersController do
   describe "GET show" do
     it "should show an answer to a question" do
       answer = Answer.create(body: "test")
-      byebug
       get :show, id: answer.id
       expect(response).to render_template("show")
     end
