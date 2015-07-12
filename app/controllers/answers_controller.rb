64f3f4acd7a0ae28714f01255f7add5f2c5e33c8
John Lyden <john.lyden86@gmail.com>
2015-07-12 15:19:50 -0400

    answers can be created and appended to questions show with ajax request.  is being appended after the #answer-form tag now, but can be changed to whereever we want the answer to show up when styling is complete

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -8,7 +8,11 @@ class AnswersController < ApplicationController
     else
       flash[:alert] = "#{@answer.errors.full_messages.join("--")}"
     end
-    redirect_to question
+    if request.xhr?
+      render :create
+    else
+      redirect_to question
+    end
   end
 
   def show
